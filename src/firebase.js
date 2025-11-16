1a7f865c2c076ac28a4c8fc41ecf5a7b7da3c76d
jaswanth6666 <jaswanthkarri0111@gmail.com>
2025-11-16 11:59:39 +0530

    Updated code

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
+// This code will now read from the secure variables you set up in Vercel
 const firebaseConfig = {
-    apiKey: "AIzaSyDaJA6Swob964_6w9-It_Av9UQzUrV1Rno",
-    authDomain: "rfid-library-system-42627.firebaseapp.com",
-    databaseURL: "https://rfid-library-system-42627-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "rfid-library-system-42627",
-    storageBucket: "rfid-library-system-42627.firebasestorage.app",
-    messagingSenderId: "711271739889",
-    appId: "1:711271739889:web:6ae022575d80eae7429450"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
